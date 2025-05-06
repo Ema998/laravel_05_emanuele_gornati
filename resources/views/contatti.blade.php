036dc1ed9136871a9deade463c1151cf7364b8a4
Ema998 <emanuele.gornati3@gmail.com>
2025-05-06 16:39:58 +0200

    commit

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -4,6 +4,29 @@
                 <div class="col-12 h-25 justify-content-center align-items-center">
                     <h3 class="text-color text-center display-5">CONTATTI</h3>
                 </div>
+                <div class="row justify-content-center align-items-center">
+                    <div class="col-12 text-center">
+                        <h4>Per qualsiasi informazione non esitare a contattarci</>
+                </div>
+                <div class="row justify-content-center align-items-center">
+                    <div class="clo-12 col-md-5">
+                    <form class="px-5 py-3 box-bg shadow rounded">
+                        <div class="mb-3">
+                            <label for="username" class="form-label">inserisci il tuo nome</label>
+                            <input type="text" class="form-control" id="username" >
+                        </div>
+                        <div class="mb-3">
+                            <label for="exampleInputEmail1" class="form-label">inserisci la tua email</label>
+                            <input type="email" class="form-control" id="email" aria-describedby="emailHelp">
+                        </div>
+                        <div class="mb-3">
+                            <label for="text" class="form-label">scrivici i tuoi suggerimenti</label>
+                            <textarea type="" class="form-control" id="message" cols="30" rows="10"></textarea>
+                        </div>
+                        <button type="submit" class="btn btn-dark">invia</button>
+                    </form>
+                    </div>
+                </div>
                 <div class="col-md-3 text-center box d-flex flex-column justify-content-center align-items-center">
                     <div class="row">
                     <div class="col-12">
