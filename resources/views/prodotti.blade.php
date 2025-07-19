bcac0aad002197ad8046f4308419ec2383d92fcc
Ema998 <emanuele.gornati3@gmail.com>
2025-07-19 11:57:57 +0200

    commit

--- resources/views/prodotti.blade.php
+++ resources/views/prodotti.blade.php
@@ -4,12 +4,17 @@
                 <div class="row">
                     <h2 class="text-center display-5">I NOSTRI PRODOTTI</h2>
                 </div>
-                @foreach ($prodotti as $prodotto)
-                    <div class="col-12 col-md-3">
-                        <x-prodottiCard prodottoName="{{ $prodotto['name'] }}" prodottoDescription="{{ $prodotto['description'] }}" prodottoPrice="{{ $prodotto['price'] }}" prodottoId="{{ $prodotto['id'] }}" />
+                @if (count($prodotti)>0)
+                    @foreach ($prodotti as $prodotto)
+                        <div class="col-12 col-md-3">
+                            <x-prodottiCard prodottoName="{{ $prodotto['name'] }}" prodottoDescription="{{ $prodotto['description'] }}" prodottoPrice="{{ $prodotto['price'] }}" prodottoId="{{ $prodotto['id'] }}" />
+                        </div>
+                    @endforeach
+                @else
+                    <div class="col-12">
+                        <p class="text-center">Non ci sono prodotti disponibili al momento.</p>
                     </div>
-                @endforeach
+                @endif
             </div>
-        </div>
         </header>
 </x-layout>
